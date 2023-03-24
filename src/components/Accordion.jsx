e4e6d9e9e27e9f2eb55963196adec5f54bb1014b
Justin <jstn.anthony1@gmail.com>
2023-03-23 22:34:45 -0400

    more styling & starting accordion menu

---
+++ src/components/Accordion.jsx
@@ -0,0 +1,60 @@
+import {useEffect, useState} from "react";
+import "./Accordion.css"
+
+const Accordion = ({items, keepOthersOpen}) => {
+
+    const [accordionItems, setAccordionItems] = useState(null)
+
+    useEffect(() => {
+        if (items) {
+            setAccordionItems([
+                ...items.map(item =>({
+                    ...item,
+                    toggled: false
+                }))
+            ])
+        }
+    }, [items])
+
+    function handleAccordionToggle(clickedItem){
+        setAccordionItems([...accordionItems.map((item) => {
+            let toggled = item.toggled;
+
+            if(clickedItem.id === item.id){
+                toggled = !item.toggled
+            } else if(!keepOthersOpen){
+                toggled = false
+            }
+
+            return {
+                ...item,
+                toggled
+            }
+        })])
+    }
+
+
+    return(
+        <div className="accordionParent">
+            {accordionItems?.map((listItem, key) =>{
+                return (
+                    <div className={`accordion ${listItem.toggled ? 'toggled' : ''}`}key={key}>
+                        <button className="toggle" onClick={() => handleAccordionToggle(listItem)}>
+                            <p>{listItem.label}</p>
+                            <div className="directionIndicator">
+                                {listItem.toggled ? '-' : '+'}
+                            </div>
+                        </button>
+                        <div className="contentParent">
+                            <div className="content">
+                                {listItem.renderContent()}
+                            </div>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Accordion;
